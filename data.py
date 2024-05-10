6be14df2fdf6a9933f1af59510d5a2c571ff4900
NNHieu <ngochieutb13@gmail.com>
2024-05-10 16:48:06 +0000

    index op, keyval op, causal tracing

---
+++ data.py
@@ -0,0 +1,130 @@
+import numpy as np
+import datasets
+from tqdm import tqdm
+import pandas as pd
+import random
+from copy import deepcopy
+
+def generate_sample(x_length, high=10, is_example=False):
+    # a = np.random.randint(size=(x_length,), low=0, high=high)
+    a = list(range(10))
+    random.shuffle(a)
+    a = a[:x_length]
+    ind = np.random.randint(low=0, high=x_length)
+    # s = f'a={a.tolist()}. a[{ind}]='
+    e = {
+        "array": a,
+        "query": ind,
+        'target': a[ind]
+    }
+    # ans = f'{a[ind]}'
+    # if is_example:
+    #     return s + ans
+    # return s, ans
+    return e
+
+def create_dataset(qlen_min=3, qlen_max=10, max_num_shot=5, num_prompt=10, num_query=100):
+    q_len_range = range(qlen_min, qlen_max + 1)
+    num_shot_range = range(0, max_num_shot + 1)
+    acc = np.zeros((len(num_shot_range), len(q_len_range)))
+
+    # Create prompts
+    data = []
+    # all_prompts = []
+    # all_answers = []
+    task_prompt = """Output the value of an array at a given index."""
+    for num_shot in tqdm(num_shot_range):
+        # task_prompt = ""
+        # task_prompt = task_prompt0
+        for q_len in q_len_range:
+            correct = 0
+            for i in range(num_prompt):
+                few_shots = []
+                for shot in range(num_shot):
+                    length = np.random.randint(3, 6)
+                    few_shots.append(generate_sample(length, is_example=True))
+                # few_shot_text += generate_sample(length, is_example=True) + "\n"
+                # print(task_prompt)
+                answers = []
+                queries = []
+                for _ in range(num_query):
+                    e = generate_sample(q_len)
+                    # queries.append(query)
+                    # answers.append(answer)
+                    e['few_shots'] = few_shots
+                    e['query_array_length'] = q_len
+                    e['num_few_shots'] = num_shot
+                    e['task_prompt'] = task_prompt
+                    data.append(e)
+                # all_prompts += [(task_prompt + q) for q in queries]
+                # all_answers += answers
+    
+    ds = datasets.Dataset.from_pandas(pd.DataFrame(data=data))
+    return ds
+
+def create_prompt(e, include_task_description=True):
+    task_description = "Output the value of an array at a given index.\n"
+
+    task_prompt = ""
+    if include_task_description:
+        task_prompt = task_description
+
+
+    def to_qtext(s, include_target=False):
+        # template = f"a = {s['array']}\nQ: a[{s['query']}]\nA: "
+        # template = f"Let a={s['array']}. The value of the array at index {s['query']} is "
+        template = f"a={s['array']}. a[{s['query']}]="
+        # template = f"a={s['array']}\nQ: a[{s['query']}]\nA: "
+        if not include_target:
+            return template
+        return template + f"{s['target']}"
+    
+    if len(e['few_shots']) == 0:
+        task_prompt += to_qtext(e)
+        e['prompt'] = task_prompt
+    else:
+        for s in e['few_shots']:
+            task_prompt += to_qtext(s, True) + "\n"
+        task_prompt += to_qtext(e)
+        e['prompt'] = task_prompt
+    return e
+
+def create_prompt_dictionary(e, include_task_description=True):
+    task_description = "Output the value of a dictionary at a given key.\n"
+
+    task_prompt = ""
+    if include_task_description:
+        task_prompt = task_description
+
+    def to_qtext(s, include_target=False):
+        # template = f"a = {s['array']}\nQ: a[{s['query']}]\nA: "
+        # template = f"Let a={s['array']}. The value of the array at index {s['query']} is "
+        keys = [
+            'a', 'b', 'c', 'd', 'e', 'f', 'h', 'i', 'j', 'k', 'l', 'm', 'n',
+        ][:len(s['array'])]
+        random.shuffle(keys)
+        d = dict(zip(keys, s['array']))
+        s_arr = "{" + ", ".join([f"{k}: {v}" for k, v in d.items()]) + "}"
+        template = f"data={s_arr}. data[{keys[s['query']]}]="
+        # template = f"a={s['array']}\nQ: a[{s['query']}]\nA: "
+        if not include_target:
+            return template
+        return template + f"{s['target']}"
+    
+    if len(e['few_shots']) == 0:
+        task_prompt += to_qtext(e)
+        e['prompt'] = task_prompt
+    else:
+        for s in e['few_shots']:
+            task_prompt += to_qtext(s, True) + "\n"
+        task_prompt += to_qtext(e)
+        e['prompt'] = task_prompt
+    return e
+
+if __name__ == '__main__':
+    from pathlib import Path
+    print(generate_sample(10))
+    output_dir = Path("datasets")
+    ds = create_dataset()
+    ds.save_to_disk(str(output_dir / f"value_at_index/v0.1"))
+    # ds.push_to_hub("nnheui/understanding-index-operation-v0.1", private=True)
---
+++ index_op.py
@@ -0,0 +1,86 @@
+from pathlib import Path
+import sys
+sys.path.append("/home/tunghoang/hieunn/pyvene")
+
+from data import generate_sample
+import torch
+import pandas as pd
+import numpy as np
+from datasets import load_from_disk
+from transformers import AutoTokenizer
+from tqdm import tqdm
+
+from vllm import LLM, SamplingParams
+from data import create_prompt
+import argparse
+
+device = "cuda:0" if torch.cuda.is_available() else "cpu"
+
+def parse_arguments():
+    """Parse command line arguments."""
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--model', type=str, default='UCLA-AGI/zephyr-7b-sft-full-SPIN-iter0')
+    parser.add_argument('--model_alias', type=str, required=True)
+    parser.add_argument('--revision', type=str, default=None)
+    parser.add_argument('--no-task-description', action="store_true")
+    parser.add_argument('--psize', type=int, default=1)
+    # parser.add_argument('--data_frac', type=int, default=0)
+    # parser.add_argument('--frac_len', type=int, default=0)
+    # parser.add_argument('--data_path', type=str, default='HuggingFaceH4/ultrachat_200k')
+    return parser.parse_args()
+args = parse_arguments()
+
+
+dataset_alias = "value_at_index_v0_1"
+ds = load_from_disk("datasets/value_at_index/v0.1")
+# ds = ds.filter(lambda e: e['query_array_length'] < 4)
+
+revision = args.revision
+if args.no_task_description:
+    output_path = Path("outputs/index-op-v0.1-wotd/")
+else:
+    output_path = Path("outputs/index-op-v0.1/")
+output_path.mkdir(exist_ok=True, parents=True)
+
+model_path = args.model
+output_path /= args.model_alias
+
+tokenizer = AutoTokenizer.from_pretrained(model_path)
+if tokenizer.pad_token is None:
+    tokenizer.pad_token = tokenizer.eos_token
+    
+# ds = ds.filter(lambda e: e['num_few_shots'] == 0)
+ds = ds.map(create_prompt,
+            fn_kwargs={"include_task_description": not args.no_task_description},
+            )
+
+print("----- Example prompt -----")
+print(ds[1000]['prompt'])
+print("----- End of Example prompt -----")
+
+llm = LLM(
+    model=model_path,
+    tensor_parallel_size=args.psize,
+    revision=revision,
+    # gpu_memory_utilization=0.4, 
+)
+sampling_params = SamplingParams(temperature=1.0, top_p=1.0, max_tokens=1, logprobs=1)
+results_gathered = llm.generate(ds['prompt'], sampling_params)
+
+# # _, pred_toks,_ = forward(all_prompts)
+# # print(query, pred_tok, answer)
+all_preds = []
+logprobs = []
+for i, result in enumerate(results_gathered):
+    pred = result.outputs[0].text.strip()
+    all_preds.append(result.outputs[0].text)
+    logprobs.append({k:(v.logprob, v.rank, v.decoded_token) for k,v in result.outputs[0].logprobs[0].items()})
+    # d += (pred == ds[i]['target'])
+    if i % 1000 == 0:
+        print('-----')
+        print(ds[i]['prompt'])
+        print(result.outputs[0].text)
+        print(pred)
+    # pred_tok = tokenizer.(result.outputs[0].text)
+# acc[num_shot, q_len - qlen_min] = correct / (num_prompt * num_query)
+torch.save({"all_preds": all_preds, "logprobs": logprobs}, output_path)
